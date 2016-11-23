c4f5c21c7d03ad1dbc4fd945838695f9d7a06fb2
Josh Gretz <jgretz@gmail.com>
2016-11-23 08:09:54 -0500

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+
+// This is a class-based component because the current
+// version of hot reloading won't hot reload a stateless
+// component at the top-level.
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.element
+};
